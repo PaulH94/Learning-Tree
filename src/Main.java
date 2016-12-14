242de11f0aea43a6ba60a1fffbe44a47d0806ca8
Paul Huynh <paulhuynh94@gmail.com>
2016-12-14 08:40:07 -0800

    Add files via upload
    A few changes

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,42 @@ public class Main{
 		if(pos == 0 || neg == 0)
 			return 0;
 		return -1*(pos*log2(pos)) - (neg*log2(neg));
-		//return log2(neg);
+
 	}
 
-	public static double kgain(){
+	public static double kgain(int[][] x){
 		//gain(system,attribute) = entropy(system) - 
 		//summation of Sv/S * entropy(Sv)
 		//where Sv/S is the ratio of the value/attribute
-
+		int count = x.length;
+		
 		return 0.0;
+	}
+
+	//Entropy of the system that is passed in
+	public static double sysepy(int[][] set){
+		int count = set.length;
+		int positive = 0;
+		for(int i = 0; i<count;++i){
+			if(set[i][set[i].length - 1] == 1)
+				++positive;
+		}
+		double pos = (double) positive/count;
+		//System.out.println(pos);
+		double neg = (double) (count-positive)/count;
+		return entropy(pos,neg);
 	}
 
 	public static void main(String[] args){
 		//System.out.println(log2(0.0));
 		System.out.println(entropy(0.99,0.01));
+		//Humidity,Wind,PlayTennis
+		int[][] example = {{1,1,0},
+							{0,0,1},
+							{1,0,1},
+							{0,0,0}};
+		kgain(example);
+		System.out.println(sysepy(example));
 	}
 
 }
