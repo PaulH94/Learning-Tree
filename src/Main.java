fffa658b733aaada3992babaaa41eb06f4b10a5e
Paul Huynh <paulhuynh94@gmail.com>
2016-12-15 09:02:12 -0800

    Add files via upload
    Able to calculate knowledge gain

--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,6 @@ public class Main{
 
 	}
 
-	public static double kgain(int[][] x){
-		//gain(system,attribute) = entropy(system) - 
-		//summation of Sv/S * entropy(Sv)
-		//where Sv/S is the ratio of the value/attribute
-		int count = x.length;
-		
-		return 0.0;
-	}
 
 	//Entropy of the system that is passed in
 	public static double sysepy(int[][] set){
@@ -35,6 +27,50 @@ public class Main{
 		return entropy(pos,neg);
 	}
 
+	public static double kgain(int[][] sys,int num){
+		//gain(system,attribute) = entropy(system) - 
+		//summation of Sv/S * entropy(Sv)
+		//where Sv/S is the ratio of the value/attribute
+		double sy = sysepy(sys);
+		int count = sys.length;
+		int high = 0;
+		for(int i = 0; i < count; ++i){
+			if(sys[i][num] == 1)
+				++high;
+		}
+
+		int[][] temph = new int[high][3];
+		int[][] templ = new int[count-high][3];
+		int ch = 0;
+		int cl = 0;
+
+		for(int i = 0; i < count; ++i){
+			if(sys[i][num] == 1){
+				temph[ch] = sys[i];
+				++ch;
+			}
+			if(sys[i][num] == 0){
+				templ[cl] = sys[i];
+				++cl;
+			}
+		}
+		double hi = (double) high/count;
+		double low = (double) (count - high)/count;
+		return sy - (hi*sysepy(temph)) - (low*sysepy(templ));
+	}
+
+	public static double mostGain(int[][] sys){
+		double best = 0;
+		double current = 0;
+		for(int i = 0; i<2; ++i){
+			current = kgain(sys,i);
+			System.out.println(current);
+			if(current > best)
+				best = current;
+		}
+		return best;
+	}
+
 	public static void main(String[] args){
 		//System.out.println(log2(0.0));
 		System.out.println(entropy(0.99,0.01));
@@ -43,8 +79,12 @@ public class Main{
 							{0,0,1},
 							{1,0,1},
 							{0,0,0}};
-		kgain(example);
-		System.out.println(sysepy(example));
+		double yo = kgain(example,1);
+		System.out.println(kgain(example,0));
+		System.out.println(yo);
+		System.out.println("------------------------");
+		System.out.println(mostGain(example));
+		//System.out.println(sysepy(example));
 	}
 
 }
