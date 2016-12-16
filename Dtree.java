d56e9787b881c2a2ae70feb848fabc0cfe7b9593
Paul Huynh <paulhuynh94@gmail.com>
2016-12-16 11:33:46 -0800

    Add files via upload
    Started working on the tree itself

---
+++ Dtree.java
@@ -0,0 +1,16 @@
+public class Dtree{
+
+	public int[] order;
+	private Node root;
+	
+
+	public Dtree(int[][] sys){
+		root = new Node(sys);
+		order = new int[3];
+	}
+
+	public void newlvl(){
+		//int[][] system = 
+		node current = root;
+	}
+}
